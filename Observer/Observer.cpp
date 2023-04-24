f719652f027d48a1bd3798da5e03b3108b8626ac
ArtemShchuchev <shuaa@rambler.ru>
2023-04-24 19:39:09 +0700

    Add files

---
+++ Observer/Observer.cpp
@@ -0,0 +1,30 @@
+﻿#include "Observer.h"
+
+void Observed::addObserver(std::weak_ptr<Observer> observer)
+{
+	observers_.push_back(observer);
+}
+
+void Observed::warning(const std::string& message) const
+{
+	for (auto& o : observers_)
+	{
+		if (auto shared_obj = o.lock()) shared_obj->onWarning(message);
+	}
+}
+
+void Observed::error(const std::string& message) const
+{
+	for (auto& o : observers_)
+	{
+		if (auto shared_obj = o.lock()) shared_obj->onError(message);
+	}
+}
+
+void Observed::fatalError(const std::string& message) const
+{
+	for (auto& o : observers_)
+	{
+		if (auto shared_obj = o.lock()) shared_obj->onFatalError(message);
+	}
+}
---
+++ Observer/Observer.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <iostream>
+#include <fstream>
+
+class Observer
+{
+public:
+    virtual ~Observer() = default;
+    virtual void onWarning(const std::string& message) {}
+    virtual void onError(const std::string& message) {}
+    virtual void onFatalError(const std::string& message) {}
+};
+
+class Observed
+{
+    std::vector<std::weak_ptr<Observer>> observers_;
+public:
+    void addObserver(std::weak_ptr<Observer> observer);
+    void warning(const std::string& message) const;
+    void error(const std::string& message) const;
+    void fatalError(const std::string& message) const;
+};
